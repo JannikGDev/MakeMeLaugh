bc4a2cd5bd439fba9c95f8302e29b8322202af8a
Anton Velmozhnyi <antvelm@gmail.com>
2024-01-27 14:23:12 +0100

    123

---
+++ CatVsSquirrels/Assets/Scripts/Guns/AProjectile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class AProjectile : MonoBehaviour
+{
+    public GameObject m_vfxPrefab;
+
+    private Rigidbody2D m_body;
+
+    private bool collided = false;
+
+    void OnEnable()
+    {
+        m_body = GetComponent<Rigidbody2D>();
+    }
+
+    public void Init(Vector3 force)
+    {
+        m_body.AddForce(force);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collided) return;
+
+        collided = true;
+
+        if (collision.contactCount > 0)
+        {
+            var contact = collision.contacts[0];
+            //contact.normal
+
+            var contactVFX = Instantiate(m_vfxPrefab, transform.position, Quaternion.LookRotation(contact.normal));
+        }
+    }
+}
---
+++ CatVsSquirrels/Assets/Scripts/Guns/Machinegun.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machinegun : MonoBehaviour
+{
+    public float m_spread = 10;
+    public float m_shotForce = 20;
+    public float m_shotDelay = 0.2f;
+
+    private float m_lastShotTime = 0;
+
+    public Transform m_shotPos;
+
+    public GameObject m_projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateInput();
+    }
+
+    private void UpdateInput()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if(m_lastShotTime + m_shotDelay > Time.time) return;
+
+            m_lastShotTime = Time.time;
+            ShotProjectile();
+        }
+    }
+
+    private void ShotProjectile()
+    {
+        // The angle in degrees by which you want to rotate the vector
+        //float rotationAngleDegrees = 45.0f;
+
+        var rotationAngleDegrees = Random.value * m_spread;
+
+        rotationAngleDegrees -= m_spread / 2f;
+
+        // Convert the rotation angle from degrees to radians
+        float rotationAngleRadians = rotationAngleDegrees * Mathf.Deg2Rad;
+
+        // Create a quaternion that represents the rotation
+        Quaternion rotationQuaternion = Quaternion.Euler(0, 0, rotationAngleDegrees);
+
+        // Rotate the original vector using the quaternion
+        Vector3 rotatedVector = rotationQuaternion * transform.right;
+
+        GameObject newProjectile = Instantiate(m_projectilePrefab, m_shotPos.position, Quaternion.identity);
+        var aProjectile = newProjectile.GetComponent<AProjectile>();
+
+        aProjectile.Init(rotatedVector * m_shotForce);
+    }
+}
