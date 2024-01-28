72fdf598666ce703e622ae3a5d1084b460c2af1c
Anton Velmozhnyi <antvelm@gmail.com>
2024-01-28 11:34:47 +0100

    explotion

---
+++ CatVsSquirrels/Assets/Scripts/VFXOnDead.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HealthComponent))]
+public class VFXOnDead : MonoBehaviour
+{
+    HealthComponent healthComponent;
+
+    public GameObject m_VFXPrefab;
+
+    public SimpleAudioEvent m_SFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthComponent = GetComponent<HealthComponent>();
+
+        healthComponent.onDead += OnDie;
+    }
+
+    void OnDie()
+    {
+        AudioManager.instance.PlayInWorld(transform.position, m_SFX);
+
+        Instantiate(m_VFXPrefab, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+}
