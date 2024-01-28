fb3066000f508c7f652109b5ab1f40f3bb8520b9
Jannik Gröger <jgr@althammer-kill.de>
2024-01-28 11:29:47 +0100

    Added FMOD Project

--- CatVsSquirrels/Assets/Scripts/AudioManager.cs
+++ CatVsSquirrels/Assets/Scripts/AudioManager.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
+
 
 public class AudioManager : MonoBehaviour
 {
@@ -8,10 +11,23 @@ public class AudioManager : MonoBehaviour
 
     public static AudioManager instance;
 
+    private void OnEnable()
+    {
+        if (AudioManager.instance != null)
+        {
+            GameObject.Destroy(this);
+        }
+        else
+        {
+            instance = this;
+            Object.DontDestroyOnLoad(this);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        instance = this;
+        
     }
 
     public void PlayInWorld(Vector3 position, SimpleAudioEvent simpleAudioEvent)
