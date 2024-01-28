bdbdbc11f0e7744c94702e46c1dc4da94e8c92e0
Anton Velmozhnyi <antvelm@gmail.com>
2024-01-28 13:49:00 +0100

    ss

---
+++ CatVsSquirrels/Assets/PlayerControls/Scripts/SquirelMeleeAttack.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquirelMeleeAttack : MonoBehaviour
+{
+    public Animator m_animator;
+    public float m_attackDelay = 2.0f;
+    public float m_attackRadius = 1;
+
+    public float m_lastAttackTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_lastAttackTime = Random.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_lastAttackTime + m_attackDelay > Time.time) return;
+
+        m_lastAttackTime = Time.time;
+
+        m_animator.SetTrigger("melee_attack");
+
+        Player.instance.GetComponent<HealthComponent>().TakeDamage(1);
+    }
+
+    public bool IsPlayerInRadius()
+    {
+        if (Vector3.Distance(Player.instance.transform.position, transform.position) < m_attackRadius)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
